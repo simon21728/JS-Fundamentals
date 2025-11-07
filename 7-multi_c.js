0b41026e75a55f49c012a7b9292db1e74b5b694c
Simon <simonbayu2211@exgmail.com>
2025-11-06 23:08:55 -0800

    I love C

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -1,13 +1,9 @@
 #!/usr/bin/node
 
-const x = parseInt(process.argv[2], 10);
+const x = Number(process.argv[2]);
 
-if (isNaN(x) || x <= 0) {
-  console.log("Missing number of occurrences");
-} else {
-  let i = 0;
-  while (i < x) {
-    console.log("C is fun");
-    i++;
-  }
+if (isNaN(x)) {
+  console.log('Missing number of occurrences');
+} else if (x > 0) {
+  for (let i = 0; i < x; i++) console.log('C is fun');
 }
